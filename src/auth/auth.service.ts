4046dc2b7cef6ec57cce63390eb164902843b7e6
Abdullah Al Mamun <amsshoyon@gmail.com>
2022-04-17 00:25:54 +0600

    Login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 
@@ -12,5 +12,10 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto) {
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+        if (!username) throw new UnauthorizedException('Invalid credentials');
     }
 }

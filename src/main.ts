3fe29e182e0a53677807ed1da0956bc40397f28a
Abdullah Al Mamun <amsshoyon@gmail.com>
2022-05-15 23:25:01 +0600

    logger added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { urlencoded, json } from 'express';
 import * as config from 'config';
 
 async function bootstrap() {
+    const logger = new Logger('bootstrap');
     const serverConfig: any = config.get('server');
     const port = process.env.PORT || serverConfig.port;
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ extended: true, limit: '50mb' }));
-    console.log(`App started on port ${port}`);
+    logger.log(`App started on port ${port}`);
     await app.listen(port);
 }
 bootstrap();
--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 import * as config from 'config';
 const oAuthConfig: any = config.get('oAuth');
 const mailConfig: any = config.get('mailing');
@@ -33,7 +34,10 @@ const transporter = nodemailer.createTransport(
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 transporter.verify((error, success) => {
-    if (error) return console.log('transporter:', error);
+    if (error) {
+        Logger.error(error);
+        throw new InternalServerErrorException();
+    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     transporter.on('token', token => {
         // Something
